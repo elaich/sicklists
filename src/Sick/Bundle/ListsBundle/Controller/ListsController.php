47f9fff921f3b507a7329b00181c928a4420a5df
El Aïch Marouane <elaich.marouane@gmail.com>
2014-09-04 05:09:41 +0100

    Add an Item

--- src/Sick/Bundle/ListsBundle/Controller/ListsController.php
+++ src/Sick/Bundle/ListsBundle/Controller/ListsController.php
@@ -3,6 +3,9 @@
 namespace Sick\Bundle\ListsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Sick\Bundle\ListsBundle\Entity\ListItem;
 
 class ListsController extends Controller
 {
@@ -14,9 +17,42 @@ class ListsController extends Controller
 
 		$items = $repository->findAll();
 
+		$item = new ListItem();
+
+		$form = $this->createFormBuilder($item)
+					->setAction($this->generateUrl('sick_lists_postitem'))
+					->add('text', 'text', array(
+						'attr' => array('placeholder' => 'Enter a task')
+					))
+					->getForm();
+
         return $this->render('SickListsBundle:Lists:index.html.twig', array(
+			'form' => $form->createView(),
 			'items' => $items
 		));    
+	}
+
+	public function addItemAction(Request $request)
+	{
+		$item = new ListItem();
+
+		$form = $this->createFormBuilder($item)
+					->setAction($this->generateUrl('sick_lists_postitem'))
+					->add('text', 'text', array(
+						'attr' => array('placeholder' => 'Enter a task')
+					))
+					->getForm();
+
+		$form->bind($request);
+
+		if ($form->isValid())
+		{
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($item);
+			$em->flush();
+		}
+
+		return $this->redirect($this->generateUrl('sick_lists_homepage'));
 	}
 
 }
--- src/Sick/Bundle/ListsBundle/Tests/Controller/ListsControllerTest.php
+++ src/Sick/Bundle/ListsBundle/Tests/Controller/ListsControllerTest.php
@@ -19,4 +19,80 @@ class ListsControllerTest extends WebTestCase
 		);
     }
 
+	public function testFormToAddItems()
+	{
+		$client = static::createClient();
+
+		$crawler = $client->request('GET', '/');
+
+		$form = $crawler->filter('form');
+
+		$this->assertGreaterThan(
+			0,
+			$form->count(),
+			"Didn't find the form"
+		);
+
+		$input = $form->filter('input[name="form[text]"]');
+
+		$this->assertEquals('Enter a task', $input->attr('placeholder'), "Add the placeholder ;)");
+	}
+
+	public function testPostItemResponse()
+	{
+		$client = static::createClient();
+
+		$client->request('POST', '/items/new', array('text' => 'Call abdelatif'));
+
+		$this->assertEquals(302, $client->getResponse()->getStatusCode());
+	}
+
+	public function testPostItemRequestRoute()
+	{
+		$client = static::createClient();
+
+		$client->request('POST', '/items/new', array('text' => 'Call abdelatif'));
+
+		$this->assertEquals('sick_lists_postitem', 
+			$client->getRequest()->attributes->get('_route'));
+	}
+
+	public function testPostItemRequestMethod()
+	{
+		$client = static::createClient();
+
+		$client->request('POST', '/items/new', array('text' => 'Call abdelatif'));
+
+		$this->assertEquals('Sick\Bundle\ListsBundle\Controller\ListsController::addItemAction',
+			$client->getRequest()->attributes->get('_controller'));
+		
+	}
+
+	public function testPostItem()
+	{
+		$client = static::createClient();
+		$client->followRedirects(true);
+
+		$crawler = $client->request('GET', '/');
+
+		$form = $crawler->filter('form')->form();
+
+		$crawler = $client->submit($form, array(
+			'form[text]' => 'Call abdelatif'
+		));
+
+		print_r($crawler->text());
+
+		$this->assertGreaterThan(
+			0,
+			$crawler->filter('html:contains("1: Buy 20 grams")')->count(),
+			'post is fchkel'
+		);
+
+		$this->assertGreaterThan(
+			0,
+			$crawler->filter('html:contains("Call abdelatif")')->count()
+		);
+	}
+
 }
