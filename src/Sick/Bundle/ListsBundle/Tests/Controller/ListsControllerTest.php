120cc924a543e83ef66ed22956fdc72478b14b67
El Aïch Marouane <elaich.marouane@gmail.com>
2014-09-04 18:18:12 +0100

    Refactoring Functional Tests and define database to test env

--- src/Sick/Bundle/ListsBundle/Tests/Controller/ListsControllerTest.php
+++ src/Sick/Bundle/ListsBundle/Tests/Controller/ListsControllerTest.php
@@ -6,24 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ListsControllerTest extends WebTestCase
 {
-    public function testIndex()
-    {
-        $client = static::createClient();
+	protected $client;
 
-        $crawler = $client->request('GET', '/');
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->client = static::createClient();
+	}
+
+	public function testIndex()
+	{
+		$crawler = $this->client->request('GET', '/');
 
 		$this->assertGreaterThan(
 			0,
 			$crawler->filter('html:contains("1: Buy 20 grams")')->count(),
 			"Didn't may be you have to do something"
 		);
-    }
+	}
 
 	public function testFormToAddItems()
 	{
-		$client = static::createClient();
-
-		$crawler = $client->request('GET', '/');
+		$crawler = $this->client->request('GET', '/');
 
 		$form = $crawler->filter('form');
 
@@ -40,48 +45,39 @@ class ListsControllerTest extends WebTestCase
 
 	public function testPostItemResponse()
 	{
-		$client = static::createClient();
+		$this->client->request('POST', '/items/new', array('text' => 'Call abdelatif'));
 
-		$client->request('POST', '/items/new', array('text' => 'Call abdelatif'));
-
-		$this->assertEquals(302, $client->getResponse()->getStatusCode());
+		$this->assertEquals(302, $this->client->getResponse()->getStatusCode());
 	}
 
 	public function testPostItemRequestRoute()
 	{
-		$client = static::createClient();
-
-		$client->request('POST', '/items/new', array('text' => 'Call abdelatif'));
+		$this->client->request('POST', '/items/new', array('text' => 'Call abdelatif'));
 
 		$this->assertEquals('sick_lists_postitem', 
-			$client->getRequest()->attributes->get('_route'));
+			$this->client->getRequest()->attributes->get('_route'));
 	}
 
 	public function testPostItemRequestMethod()
 	{
-		$client = static::createClient();
-
-		$client->request('POST', '/items/new', array('text' => 'Call abdelatif'));
+		$this->client->request('POST', '/items/new', array('text' => 'Call abdelatif'));
 
 		$this->assertEquals('Sick\Bundle\ListsBundle\Controller\ListsController::addItemAction',
-			$client->getRequest()->attributes->get('_controller'));
-		
+			$this->client->getRequest()->attributes->get('_controller'));
+
 	}
 
 	public function testPostItem()
 	{
-		$client = static::createClient();
-		$client->followRedirects(true);
+		$this->client->followRedirects(true);
 
-		$crawler = $client->request('GET', '/');
+		$crawler = $this->client->request('GET', '/');
 
 		$form = $crawler->filter('form')->form();
 
-		$crawler = $client->submit($form, array(
+		$crawler = $this->client->submit($form, array(
 			'form[text]' => 'Call abdelatif'
 		));
-
-		print_r($crawler->text());
 
 		$this->assertGreaterThan(
 			0,
@@ -93,6 +89,16 @@ class ListsControllerTest extends WebTestCase
 			0,
 			$crawler->filter('html:contains("Call abdelatif")')->count()
 		);
+
+		// deleting the just added item
+		
+		$em = $this::$kernel->getContainer()->get('doctrine')->getManager();
+		$repository = $em->getRepository('SickListsBundle:ListItem');
+		$itemsToBeDeleted = $repository->findBy(array('text' => 'Call abdelatif'));
+
+		foreach ($itemsToBeDeleted as $item)
+			$em->remove($item);
+		$em->flush();
 	}
 
 }
